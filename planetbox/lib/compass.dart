a622b4a412adfc74316dab597d02f8d3b4257813
ishibashikeita <109339217+ishibashikeita@users.noreply.github.com>
2023-12-06 10:12:58 +0900

    Update compass.dart

--- planetbox/lib/compass.dart
+++ planetbox/lib/compass.dart
@@ -14,6 +14,7 @@ class _CompassState extends State<Compass> {
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
